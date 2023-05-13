c9624f62f6e954ccb9fafdbb7882d165b365a961
dristosun <107073369+dristosun@users.noreply.github.com>
2023-05-13 16:05:05 +0300

    Configure webpack and create header in Javascript

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import './styles.css';
+
+function homePage() {
+
+    const header = document.createElement('div');
+    header.classList = 'header';
+    const title = document.createElement('span');
+    title.textContent = 'SUNCAFE';
+    title.classList = 'title';
+    const home = document.createElement('span');
+    home.textContent = 'HOME';
+    home.classList = 'home normalHeading';
+    const menu = document.createElement('span');
+    menu.textContent = 'MENU';
+    menu.classList = 'menu normalHeading';
+    const contact = document.createElement('span');
+    contact.textContent = 'CONTACT';
+    contact.classList = 'contact normalHeading';
+
+    header.append(title, home, menu, contact);
+
+    return header;
+}
+
+document.body.appendChild(homePage());
