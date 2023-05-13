861bf34f45595274587ee48f89ededb9f5f1a670
dristosun <107073369+dristosun@users.noreply.github.com>
2023-05-13 18:49:47 +0300

    Create main body and style it

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import './styles.css';
+import imageOne from './coffee.jpg'
+import imageTwo from './cafe.jpg'
 
 function homePage() {
+    const homePage = document.createElement('div');
+    homePage.classList = 'homepage';
 
     const header = document.createElement('div');
     header.classList = 'header';
@@ -19,7 +23,32 @@ function homePage() {
 
     header.append(title, home, menu, contact);
 
-    
+    const main = document.createElement('div');
+    main.classList = 'main';
+    let message = document.createElement('p');
+    message.innerHTML = "Are you ready to taste the best coffee in town? <p> It's just a couple clicks away! </p>"
+    message.classList = 'message';
+
+    const containerOne = document.createElement('div');
+    containerOne.classList = 'containerOne';
+    const image1 = new Image();
+    image1.src = imageOne;
+    image1.classList = 'imageOne';
+
+    containerOne.append(message, image1);
+
+    const containerTwo = document.createElement('div');
+    containerTwo.classList = 'containerTwo';
+    const image2 = new Image();
+    image2.src = imageTwo;
+    image2.classList = 'imageTwo';
+    containerTwo.appendChild(image2);
+
+    main.append(containerOne, containerTwo);
+
+    homePage.append(header, main);
+    return homePage;
 }
 
-document.body.appendChild(homePage());
+const output = document.querySelector('#content')
+output.appendChild(homePage());
