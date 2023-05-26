a493d150b5dfa5b372c6cbe90e9f788c198937c7
dristosun <107073369+dristosun@users.noreply.github.com>
2023-05-26 22:32:50 +0300

    Finish contact page

--- src/contact.js
+++ src/contact.js
@@ -10,6 +10,16 @@ export default function contactPage() {
     const container = document.createElement('div');
     container.classList = 'contactContainer';
 
+    const location = document.createElement('div');
+    location.innerHTML = '<p class="heading">Location</p> 17 SunStreet <br> 1234 The Milky Way';
+
+    const hours = document.createElement('div');
+    hours.innerHTML = '<p class="heading"> Hours</p> 7am- 10pm Monday to Friday <br> 9am - 9pm Saturday <br> Closed Sunday';
+
+    const contact = document.createElement('div');
+    contact.innerHTML = '<p class="heading">Contact</p> 1234-555-678 <br> sunCafe@yahoo.com';
+
+    container.append(location, hours, contact);
     main.append(container);
     contactPage.append(header('contact'), main,  footer());
 
