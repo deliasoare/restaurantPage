16ba73415903894ce7be6550b1a494b07508165d
dristosun <107073369+dristosun@users.noreply.github.com>
2023-05-26 22:13:44 +0300

    Remove scrollbar

--- src/contact.js
+++ src/contact.js
@@ -4,7 +4,14 @@ export default function contactPage() {
     const contactPage = document.createElement('div');
     contactPage.classList = 'contactpage page';
 
-    contactPage.append(header('contact'), footer());
+    const main = document.createElement('div');
+    main.classList = 'mainContact';
+
+    const container = document.createElement('div');
+    container.classList = 'contactContainer';
+
+    main.append(container);
+    contactPage.append(header('contact'), main,  footer());
 
     return contactPage;
 }
